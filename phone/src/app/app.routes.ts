e2a78ee15103423e43c3c8ab9c4ca99523016d3c
JadeLuke <jadeluke.booysen@gmail.com>
2024-09-16 10:19:27 +0200

    added pages, navbar and bottom nav

---
+++ phone/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+export const routes: Routes = [
+    {path:"", component: LandingComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: SignUpComponent},
+    {path: "dashboard", component: DashboardComponent},
+    {path: "about", component: AboutComponent},
+    {path: "gallery", component: GalleryComponent},
+    {path: "settings", component: SettingsComponent},
+    {path: "contact", component: ContactComponent}
+];
